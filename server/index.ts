13c079c2bffd18d9a9788b72397f95b1d561b2e6
Shafi327 <shafishangla327@gmail.com>
2025-04-11 06:53:14 +0500

    new Deploy code with the github action

--- server/index.ts
+++ server/index.ts
@@ -30,7 +30,6 @@ app.use(cookieParser());
 // }
 app.use(cors());
 
-// api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
